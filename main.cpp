a27e6415b989cea7afcfc354f0f42f82b412b3fe
knopkloop <hruslan2007@gmail.com>
2025-11-10 12:40:13 +0300

    add for cycle and skeleton for struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+struct IntArray
+{
+  void add(int i);
+  int get(size_t id) const;
+  size_t size() const;
+};
 
 int main()
 {
@@ -8,5 +14,15 @@ int main()
   if (!std::cin)
   {
     return 1;
+  }
+  IntArray a;
+  a.add(next);
+
+
+
+  for (size_t i = 0; i < a.size(); ++i)
+  {
+    int d = a.get(i);
+    //...
   }
 }
